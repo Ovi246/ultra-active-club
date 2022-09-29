c92c47da0e5fa83513ac1a25e9a2b55c9a674105
sbswag9@gmail.com <sbswag9@gmail.com>
2022-09-29 23:25:10 +0600

    made simple responsive

--- src/App.js
+++ src/App.js
@@ -50,21 +50,19 @@ function App() {
     <div className="">
       <div className="flex flex-row">
         <div className="basis-3/4">
-          <div className="container mx-auto px-32 py-28">
+          <div className="container mx-auto px-5 py-20 md:px-32 md:py-28">
             <img
               src="images/logo.jpg"
               alt=""
-              width="70px"
-              height="70px"
-              className="inline"
+              className="inline w-5 h-5 md:w-16 md:h-16"
             />
-            <h3 className="text-3xl font-bold inline">
+            <h3 className="text-xl md:text-3xl font-bold md:inline">
               Zero to Hero Fitness Club
             </h3>
-            <h2 className="text-xl font-semibold py-5">
+            <h2 className="text-md md:text-xl font-semibold py-5">
               Choose your appropriate exercise
             </h2>
-            <div className="grid grid-cols-3 gap-5">
+            <div className="flex flex-col md:grid md:grid-cols-3 gap-5 ">
               {loading
                 ? "loading"
                 : data.map((item, idx) => (
@@ -78,7 +76,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="basis-1/4 bg-[#E7F6F2] flex flex-col gap-10 p-10 info sticky h-screen top-0">
+        <div className="basis-1/4 bg-[#E7F6F2] flex flex-col gap-2 md:gap-10 p-2 md:p-10 info sticky h-screen top-0">
           <div className="flex justify-evenly">
             <img src="images/user.jpg" alt="" width="50px" height="50px" />
             <div>
@@ -86,7 +84,7 @@ function App() {
               <p>Bashabo,Dhaka</p>
             </div>
           </div>
-          <div className="bg-[#A5C9CA] p-4 rounded-md flex justify-evenly">
+          <div className="bg-[#A5C9CA] p-2 md:p-4 rounded-md flex justify-evenly">
             <div>
               <p>60kg</p>
               <p>Weight</p>
@@ -101,7 +99,7 @@ function App() {
             </div>
           </div>
           <h6 className="mt-5">Add Break</h6>
-          <div className="bg-[#A5C9CA] p-4 rounded-md flex justify-evenly">
+          <div className="bg-[#A5C9CA] p-2 md:p-4 rounded-md flex justify-evenly">
             <p onClick={() => handleBreakTime(10)} className="cursor-pointer">
               10s
             </p>
@@ -128,7 +126,7 @@ function App() {
             <span>{breakTime}</span>
           </div>
           <button
-            className="bg-[#395B64] p-5 text-[#E7F6F2] hover:bg-[#A5C9CA] hover:text-[#395B64] mt-5"
+            className="bg-[#395B64] p-2 md:p-5 text-[#E7F6F2] hover:bg-[#A5C9CA] hover:text-[#395B64] mt-5"
             onClick={showToastMessage}
           >
             Mark As Complete
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ import React from "react";
 const Card = ({ item, handleClick, idx }) => {
   return (
     <div className="flex flex-col gap-5 bg card">
-      <img src={item.img} alt="" />
+      <img src={item.img} alt="" className="w-full h-[50%]" />
       <h3 className="text-xl">{item.name}</h3>
       <p className="text-md h-[50%]">{item.info}</p>
       <span>Age: {item.age}</span>
       <span>Per Set Time: {item.time}s</span>
       <button
-        className="bg-[#395B64] p-5 text-[#E7F6F2] hover:bg-[#E7F6F2] hover:text-[#395B64]"
+        className="bg-[#395B64] p-2 md:p-5 text-[#E7F6F2] hover:bg-[#E7F6F2] hover:text-[#395B64]"
         onClick={() => handleClick(item.time, idx)}
       >
         Add Workout

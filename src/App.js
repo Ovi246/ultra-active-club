c559abc596a94bdb6bfa09a2c574a42ff4d41554
sbswag9@gmail.com <sbswag9@gmail.com>
2022-09-29 20:38:39 +0600

    cleaned up directory, installed tailwind and toastify, created header section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/images/logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="flex flex-row">
+        <div className="basis-3/4">
+          <div className="container mx-auto px-32 py-28">
+            <img
+              src={logo}
+              alt=""
+              width="70px"
+              height="70px"
+              className="inline"
+            />
+            <h3 className="text-3xl font-bold text-blue-700 inline">
+              Zero to Hero Fitness Club
+            </h3>
+            <h2 className="text-xl font-semibold text-blue-700 py-5">
+              Choose your appropriate exercise
+            </h2>
+          </div>
+        </div>
+        <div className="basis-1/4">info</div>
+      </div>
     </div>
   );
 }
